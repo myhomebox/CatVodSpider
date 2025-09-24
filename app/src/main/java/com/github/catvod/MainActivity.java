f4e78c6109313ca3aa4bbb06d3046df14461cde3
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-09-25 00:03:08 +0800

    Update MainActivity.java

--- app/src/main/java/com/github/catvod/MainActivity.java
+++ app/src/main/java/com/github/catvod/MainActivity.java
@@ -1,60 +1,52 @@
-package com.github.catvod;
+package com.github.catvod.debug;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.github.catvod.R;
 import com.github.catvod.crawler.Spider;
-import com.github.catvod.databinding.ActivityMainBinding;
 import com.github.catvod.spider.Init;
 import com.github.catvod.spider.MQiTV;
-import com.github.catvod.spider.PTT;
 import com.github.catvod.spider.Proxy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends Activity {
 
-    private ActivityMainBinding binding;
     private ExecutorService executor;
     private Spider spider;
-    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        gson = new GsonBuilder().setPrettyPrinting().create();
+        setContentView(R.layout.activity_main);
+        Button homeContent = findViewById(R.id.homeContent);
+        Button homeVideoContent = findViewById(R.id.homeVideoContent);
+        Button categoryContent = findViewById(R.id.categoryContent);
+        Button detailContent = findViewById(R.id.detailContent);
+        Button playerContent = findViewById(R.id.playerContent);
+        Button searchContent = findViewById(R.id.searchContent);
+        Button liveContent = findViewById(R.id.liveContent);
+        Button proxy = findViewById(R.id.proxy);
+        homeContent.setOnClickListener(view -> executor.execute(this::homeContent));
+        homeVideoContent.setOnClickListener(view -> executor.execute(this::homeVideoContent));
+        categoryContent.setOnClickListener(view -> executor.execute(this::categoryContent));
+        detailContent.setOnClickListener(view -> executor.execute(this::detailContent));
+        playerContent.setOnClickListener(view -> executor.execute(this::playerContent));
+        searchContent.setOnClickListener(view -> executor.execute(this::searchContent));
+        liveContent.setOnClickListener(view -> executor.execute(this::liveContent));
+        proxy.setOnClickListener(view -> executor.execute(this::proxy));
         Logger.addLogAdapter(new AndroidLogAdapter());
         executor = Executors.newCachedThreadPool();
         executor.execute(this::initSpider);
-        spider = new PTT();
-        initView();
-        initEvent();
-    }
-
-    private void initView() {
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-    }
-
-    private void initEvent() {
-        binding.home.setOnClickListener(view -> executor.execute(this::homeContent));
-        binding.homeVideo.setOnClickListener(view -> executor.execute(this::homeVideoContent));
-        binding.category.setOnClickListener(view -> executor.execute(this::categoryContent));
-        binding.detail.setOnClickListener(view -> executor.execute(this::detailContent));
-        binding.player.setOnClickListener(view -> executor.execute(this::playerContent));
-        binding.search.setOnClickListener(view -> executor.execute(this::searchContent));
-        binding.live.setOnClickListener(view -> executor.execute(this::liveContent));
-        binding.proxy.setOnClickListener(view -> executor.execute(this::proxy));
     }
 
     private void initSpider() {
@@ -69,8 +61,7 @@ public class MainActivity extends Activity {
 
     public void homeContent() {
         try {
-            String result = gson.toJson(JsonParser.parseString(spider.homeContent(true)));
-            Init.post(() -> binding.result.setText(result));
+            Logger.t("homeContent").d(spider.homeContent(true));
         } catch (Throwable e) {
             e.printStackTrace();
         }
@@ -78,8 +69,7 @@ public class MainActivity extends Activity {
 
     public void homeVideoContent() {
         try {
-            String result = gson.toJson(JsonParser.parseString(spider.homeVideoContent()));
-            Init.post(() -> binding.result.setText(result));
+            Logger.t("homeVideoContent").d(spider.homeVideoContent());
         } catch (Throwable e) {
             e.printStackTrace();
         }
@@ -90,8 +80,7 @@ public class MainActivity extends Activity {
             HashMap<String, String> extend = new HashMap<>();
             extend.put("c", "19");
             extend.put("year", "2024");
-            String result = gson.toJson(JsonParser.parseString(spider.categoryContent("3", "2", true, extend)));
-            Init.post(() -> binding.result.setText(result));
+            Logger.t("categoryContent").d(spider.categoryContent("3", "2", true, extend));
         } catch (Throwable e) {
             e.printStackTrace();
         }
@@ -99,8 +88,7 @@ public class MainActivity extends Activity {
 
     public void detailContent() {
         try {
-            String result = gson.toJson(JsonParser.parseString(spider.detailContent(List.of("78702"))));
-            Init.post(() -> binding.result.setText(result));
+            Logger.t("detailContent").d(spider.detailContent(Arrays.asList("78702")));
         } catch (Throwable e) {
             e.printStackTrace();
         }
@@ -108,8 +96,7 @@ public class MainActivity extends Activity {
 
     public void playerContent() {
         try {
-            String result = gson.toJson(JsonParser.parseString(spider.playerContent("", "382044/1/78", new ArrayList<>())));
-            Init.post(() -> binding.result.setText(result));
+            Logger.t("playerContent").d(spider.playerContent("", "382044/1/78", new ArrayList<>()));
         } catch (Throwable e) {
             e.printStackTrace();
         }
@@ -117,8 +104,7 @@ public class MainActivity extends Activity {
 
     public void searchContent() {
         try {
-            String result = gson.toJson(JsonParser.parseString(spider.searchContent("我的人间烟火", false)));
-            Init.post(() -> binding.result.setText(result));
+            Logger.t("searchContent").d(spider.searchContent("我的人间烟火", false));
         } catch (Throwable e) {
             e.printStackTrace();
         }
@@ -126,8 +112,7 @@ public class MainActivity extends Activity {
 
     public void liveContent() {
         try {
-            String result = gson.toJson(JsonParser.parseString(spider.liveContent("")));
-            Init.post(() -> binding.result.setText(result));
+            Logger.t("liveContent").d(spider.liveContent(""));
         } catch (Throwable e) {
             e.printStackTrace();
         }
